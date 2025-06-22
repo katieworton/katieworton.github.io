7a9dad450200a52ef8cfb863481d83348cc2e0f8
Katie Worton <20370642+katieworton@users.noreply.github.com>
2025-06-23 00:57:00 +0100

    Add projects section

--- src/content.config.ts
+++ src/content.config.ts
@@ -15,4 +15,18 @@ const blog = defineCollection({
 	}),
 });
 
-export const collections = { blog };
+const projects = defineCollection({
+	// Load Markdown and MDX files in the `src/content/projects/` directory.
+	loader: glob({ base: './src/content/projects', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: ({ image }) => z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: image().optional(),
+	}),
+});
+
+export const collections = { blog, projects };
