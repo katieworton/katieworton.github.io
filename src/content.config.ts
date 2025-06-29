2ef585ac73f11f71518bb59afabac237ad1bc454
Katie Worton <20370642+katieworton@users.noreply.github.com>
2025-06-29 21:19:03 +0100

    Add optional alt text to blog post hero image

--- src/content.config.ts
+++ src/content.config.ts
@@ -12,6 +12,7 @@ const blog = defineCollection({
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: image().optional(),
+		heroImageAlt: z.string().optional(),
 	}),
 });
 
